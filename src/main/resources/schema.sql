2a7097e6cba5aca8fb8f37e546bb0399d7822924
0xRogueX <dev.vivek.dadhaniya@gmail.com>
2026-03-31 21:55:49 +0530

    fix bugs

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+-- Create sequence for Loan Applications
+CREATE SEQUENCE IF NOT EXISTS application_number_seq START 1 INCREMENT 1;
+
+-- Create sequence for Approved Loans
+CREATE SEQUENCE IF NOT EXISTS loan_number_seq START 1 INCREMENT 1;
